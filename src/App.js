2f68f66f6ad55947bc8b9b983d6d2855d896a192
andreiroman1124 <andreiroman.bt@gmail.com>
2020-01-16 14:50:17 +0200

    (WIP) - Updated code using JSX

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, `Pet Name: ${props.name}`),
-    React.createElement("h2", {}, `Type: ${props.type}`),
-    React.createElement("h2", {}, `Breed: ${props.breed}`)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 class App extends React.Component {
-  handeTitleClick() {
-    alert("You'll need to insert a <Link> here");
-  }
-
   render() {
-    return React.createElement("div", {}, [
-      React.createElement("h1", { onClick: this.handeTitleClick }, "Adopt me!"),
-      React.createElement(Pet, {
-        name: "Roger",
-        type: "dog",
-        breed: "labrador"
-      }),
-      React.createElement(Pet, {
-        name: "Roger",
-        type: "dog",
-        breed: "labrador"
-      })
-    ]);
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Roger" type="dog" breed="Labrador" />
+        <Pet name="Ben" type="cat" breed="Persian" />
+        <Pet name="Pepper" type="bird" breed="Mixed" />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
