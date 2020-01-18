8a015acd614a193cc715b0e9e2e099f5c86e9fc1
andreiroman1124 <andreiroman.bt@gmail.com>
2020-01-18 11:28:42 +0200

    (WIP) - Installed babel & modified code

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,62 @@
 import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+  state = {
+    loading: true
+  };
+  componentDidMount() {
+    petfinder.pet
+      .get({
+        output: "full",
+        id: this.props.id
+      })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed;
+        if (Array.isArray(data.petfinder.pet.breeds.breed)) {
+          breed = pet.breed.join(", ");
+        } else {
+          breed = pet.breed;
+        }
+
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
   render() {
-    return <h1>hi lol</h1>;
+    if (this.state.loading) {
+      return <h1>loading ...</h1>;
+    }
+    const { animal, breed, location, description } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
